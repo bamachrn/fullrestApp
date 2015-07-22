9ad5a62689fa989848237cde4d3c20bf7b9c240f
Bamacharan Kundu <bamachrn@localhost.localdomain>
2015-07-22 09:10:41 +0530

    added complete flow of service station

--- public/controllers/ServiceStations.js
+++ public/controllers/ServiceStations.js
@@ -1,45 +1,19 @@
-var ssApp = angular.module('ssApp',[]);
-ssApp.controller('servicestationCtrl',['$scope','$http',function($scope,$http){
-        console.log("hello world from Service Station controller");
+var ssControllers = angular.module('ssControllers',[]);
+ssControllers.controller('ssListCtrl',
+    ['$scope','$http',
+    function($scope,$http){
+       // console.log("hello world from Service Station controller");
             
-            $http.get('/ServiceStations').success(function(service_stations){
-                        console.log("got the data which is requested  :)  ");
-                                $scope.service_stations = service_stations; 
-                    });
-                
-                $scope.addBook = function(){
-                            console.log($scope.book);
-                                    $http.post('/api/Books',$scope.book).success(function(response){
-                                                    console.log("Inside Add Contact Function");
-                                                            //$scope.books = [];
-                                                            //          //books = response
-                                                            //                      
-                                                            //                              });
-                                                            //                                  };
-                                                            //                                      
-                                                            //                                          
-                                                            //                                                $scope.removebook = function(bookId){
-                                                            //                                                        console.log("Inside Remove Function");
-                                                            //                                                                console.log("Requested ID is delievered");
-                                                            //                                                                        console.log(bookId);
-                                                            //                                                                              $http.delete('/api/Books/' + bookId)
-                                                            //                                                                                  console.log("Book Deleted");
-                                                            //                                                                                       };
-                                                            //                                                                                           
-                                                            //                                                                                               
-                                                            //                                                                                                    $scope.edit = function(id){
-                                                            //                                                                                                             console.log(id);
-                                                            //                                                                                                                      $http.get('/api/Books/' + id).success(function(response){
-                                                            //                                                                                                                                   console.log("Inside Edit function");
-                                                            //                                                                                                                                               $scope.book = response; 
-                                                            //                                                                                                                                                        });
-                                                            //                                                                                                                                                             };
-                                                            //                                                                                                                                                                 
-                                                            //                                                                                                                                                                      $scope.update = function(){
-                                                            //                                                                                                                                                                              console.log($scope.book._id);
-                                                            //                                                                                                                                                                                       $http.put('/api/Books/' + $scope.book._id,$scope.book).success(function(response){
-                                                            //                                                                                                                                                                                                    console.log("Inside Update Function");
-                                                            //                                                                                                                                                                                                                 console.log(response);
-                                                            //                                                                                                                                                                                                                          });
-                                                            //                                                                                                                                                                                                                              };  
-                                                            //                                                                                                                                                                                                                              }]);
+        $http.get('/ServiceStations').success(function(service_stations){
+            //console.log("got the data which is requested  :)  ");
+            $scope.service_stations = service_stations; 
+        });
+        
+    }]);
+ssControllers.controller('ssDetailsCtrl',
+    ['$scope','$http','$routeParams',
+    function($scope,$http,$routeParams){
+        $http.get('/ServiceStations/'+$routeParams.ss_id).success(function(service_station){
+            $scope.service_station=service_station;
+        })
+    }]);
---
+++ public/controllers/ssApp.js
@@ -0,0 +1,20 @@
+var ssApp = angular.module('ssApp',[
+    'ngRoute',
+    'ssControllers'
+]);
+ssApp.config(['$locationProvider','$routeProvider',
+    function($locationProvider,$routeProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider.
+            when('/ServiceStations', {
+                templateUrl: 'views/SeviceStations.html',
+                controller: 'ssListCtrl'
+            }).
+            when('/ServiceStations/:ss_id', {
+                templateUrl: 'views/SeviceStationDetails.html',
+                controller: 'ssDetailsCtrl'
+            }).
+            otherwise({
+                redirectTo: '/ServiceStations'
+            });
+    }]);
