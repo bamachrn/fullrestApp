a4155d2853d7a106de3e84c43fff8dcbb4f23c4b
Bamacharan Kundu <bamacharank@cybage.com>
2015-08-28 20:56:40 +0530

    Service booking user creation and booking registryexcept bike_id done

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-module.exports = function(app,uri)
+module.exports = function(app,sequelize)
 {
     router.route('/')
         .get(function(req,res){
@@ -13,22 +13,24 @@ module.exports = function(app,uri)
             });
         })
         .post(function(req,res){
-            var customer = new app.models.Customer();
-            customer.first_name = req.body.first_name;
-            customer.last_name  = req.body.last_name;
-            customer.mobile_no  = req.body.mobile_no;
-            console.log(customer);
-            customer.save(function(err,customer){
-                if(err)
-                    res.send(err);
-                else
-                    res.json(customer);
+            var newCustomer = {
+                first_name:req.body.first_name,
+                last_name:req.body.last_name,
+                mobile:req.body.mobile,
+                email:req.body.email,
+                gender:req.body.gender,
+                password:req.body.password
+            }
+            //console.log(customer);
+            app.models.Customers.create(newCustomer).then(function(customer){
+                res.json(customer);
+                //console.log(customer);
             });
         });
 
     router.route('/:cust_id')
         .get(function(req,res){
-            app.models.Customer.findById(req.params.cust_id,function(err,customer){
+            app.models.Customers.findById(req.params.cust_id,function(err,customer){
                 if(err)
                     res.send(err);
                 else
@@ -36,7 +38,7 @@ module.exports = function(app,uri)
             });
         })
         .put(function(req,res){
-            app.models.Customer.findById(req.params.cust_id,function(err,customer){
+            app.models.Customers.findById(req.params.cust_id,function(err,customer){
                 if(err)
                     res.send(err);
                 customer.first_name = req.body.first_name;
@@ -49,11 +51,19 @@ module.exports = function(app,uri)
             });
         })
         .delete(function(req,res){
-            app.models.Customer.remove({_id:req.params.cust_id},function(err,customer){
+            app.models.Customers.remove({_id:req.params.cust_id},function(err,customer){
                 if(err)
                     res.send(err);
                 else
                     res.json(customer);
+            });
+        })
+        .post(function(req,res){
+            app.models.Customers.findOne({ where: {email:req.body.email}}).then(function(err,customer){
+                if(err)
+                    res.send(err);
+                else
+                    res.send(customer);
             });
         });
 
